0f55d2c45326183480d1c79ae0bf47a809e82466
Irfan Togashi <96120619+datbadwolf97@users.noreply.github.com>
2021-12-19 13:01:30 +0700

    Updated install.sh
    New method on creating download directory, it uses bash orders instead of '&&....

--- install.sh
+++ install.sh
@@ -4,6 +4,13 @@ echo Installing dependencies...
 
 apt update -y && apt upgrade -y && apt install figlet ffmpeg python -y
 pip install youtube-dl
-cd /data/data/com.termux/files/home/storage/shared && mkdir YTMSDL && cd && cd ytmsdl
 
-echo "Installed!"
+#Creating YTMSDL directory, it'll be found on /storage/shared
+echo "Creating YTMSDL download folder, can be found on SD Card via any file explorers"
+cd
+cd storage/shared
+mkdir YTMSDL
+cd
+cd ytmsdl-termux
+
+echo "Installed, run ./ytmsdl to start downloading!"
